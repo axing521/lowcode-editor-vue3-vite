e3c65e216dc40e9efcc778315895404121ea2a40
lyc-chengzi <305213781@qq.com>
2022-11-21 13:30:36 +0800

    inputSelector改用usePropsProxy读取属性

--- packages/tdp-editor/src/selectors/propSelectors/inputSelector.vue
+++ packages/tdp-editor/src/selectors/propSelectors/inputSelector.vue
@@ -1,39 +1,27 @@
 <template>
-    <a-input v-model:value="_value"></a-input>
+    <a-input v-model:value="inputValue"></a-input>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+
 export default defineComponent({
     name: EnumSelectorName.input,
 });
 </script>
 <script lang="ts" setup>
-import { computed } from 'vue';
 import type { IDesignerComponent, IPropsConfig } from 'tdp-editor-types/interface/designer';
+import { usePropsProxy } from 'tdp-editor-utils/propsFactory';
 import { EnumSelectorName } from 'tdp-editor-types/enum/designer';
-import { propsFactory } from 'tdp-editor-utils';
 import { EnumPropsValueType } from 'tdp-editor-types/enum/components';
 
 const _props = defineProps<{
     element: IDesignerComponent;
     prop: IPropsConfig;
 }>();
-
-const _value = computed<string>({
-    get(): string {
-        return propsFactory.getPropsValue(_props.element, _props.prop.key);
-    },
-    set(value: string) {
-        propsFactory.setPropsValue(
-            _props.element,
-            _props.prop.key,
-            value || '',
-            EnumPropsValueType.string
-        );
-    },
-});
-const name = EnumSelectorName.input;
-defineExpose({
-    name,
-});
+const inputValue = usePropsProxy(
+    _props.element,
+    _props.prop.key as string,
+    '',
+    EnumPropsValueType.string
+);
 </script>
