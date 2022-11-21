1c3e8fac357f8591fed875229c34b93b7d3e4e9d
lyc-chengzi <305213781@qq.com>
2022-11-21 13:33:29 +0800

    switchSelector改用usePropsProxy读取属性

--- packages/tdp-editor/src/selectors/propSelectors/switchSelector.vue
+++ packages/tdp-editor/src/selectors/propSelectors/switchSelector.vue
@@ -1,5 +1,5 @@
 <template>
-    <a-switch v-model:checked="_value" />
+    <a-switch v-model:checked="switchValue" />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -8,27 +8,20 @@ export default defineComponent({
 });
 </script>
 <script lang="ts" setup>
-import { computed } from 'vue';
 import type { IDesignerComponent, IPropsConfig } from 'tdp-editor-types/interface/designer';
 import { EnumSelectorName } from 'tdp-editor-types/enum/designer';
 import { EnumPropsValueType } from 'tdp-editor-types/enum/components';
-import propsFactory from 'tdp-editor-utils/propsFactory';
+import { usePropsProxy } from 'tdp-editor-utils/propsFactory';
 
 const _props = defineProps<{
     element: IDesignerComponent;
     prop: IPropsConfig;
 }>();
-const _value = computed({
-    get(): boolean {
-        return propsFactory.getPropsValue(_props.element, _props.prop.key) || false;
-    },
-    set(checked: boolean) {
-        propsFactory.setPropsValue(
-            _props.element,
-            _props.prop.key,
-            checked,
-            EnumPropsValueType.boolean
-        );
-    },
-});
+
+const switchValue = usePropsProxy(
+    _props.element,
+    _props.prop.key as string,
+    false,
+    EnumPropsValueType.boolean
+);
 </script>
