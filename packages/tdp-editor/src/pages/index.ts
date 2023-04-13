def2434d1911f9c33469bf05c3ec62512a6b7821
liuyc14 <liuyc14@lenovo.com>
2023-04-13 15:20:25 +0800

    修改引用名称

--- packages/tdp-editor/src/pages/index.ts
+++ packages/tdp-editor/src/pages/index.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import { initEditor } from '../editor/utils';
-import TdpPreview from './AppIndex.vue';
+import EditorIndex from './AppIndex.vue';
 import '../editor/index.less';
 import 'tdp-editor-components/src/styles/index.less';
 
-const app = createApp(TdpPreview);
+const app = createApp(EditorIndex);
 initEditor(app);
 // 渲染editor
 app.mount('#app');
