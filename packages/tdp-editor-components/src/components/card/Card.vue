4b06e14711512f0b8572c670b4818bc098fd980c
liuyc14 <liuyc14@lenovo.com>
2023-05-05 21:53:52 +0800

    优化card组件弹性布局

--- packages/tdp-editor-components/src/components/card/Card.vue
+++ packages/tdp-editor-components/src/components/card/Card.vue
@@ -39,7 +39,7 @@ const flexCardClass = computed(() => {
     }
     // 是否可以换行
     if (allProps.props.wrap) {
-        result.push('card-flex-nowrap');
+        result.push('card-flex-wrap');
     }
     return result;
 });
@@ -54,6 +54,7 @@ const flexCardClass = computed(() => {
     &.card-flex {
         display: flex;
         flex-direction: row;
+        flex-wrap: nowrap;
         justify-content: space-evenly;
         align-items: center;
         > .@{prefixName}-card {
@@ -69,8 +70,8 @@ const flexCardClass = computed(() => {
             width: 100%;
         }
     }
-    &.card-flex-nowrap {
-        flex-wrap: nowrap;
+    &.card-flex-wrap {
+        flex-wrap: wrap;
     }
 }
 /* 处理编辑时flex样式保持统一 */
@@ -78,6 +79,7 @@ const flexCardClass = computed(() => {
     > .layout-drag {
         display: flex;
         flex-direction: row;
+        flex-wrap: nowrap;
         justify-content: space-evenly;
         align-items: center;
         > .@{prefixName}-card {
@@ -91,6 +93,11 @@ const flexCardClass = computed(() => {
         > .@{prefixName}-card {
             width: 100%;
         }
+    }
+}
+.@{prefixName}-card.card-flex.card-flex-wrap.editor-designer-comp {
+    > .layout-drag {
+        flex-wrap: wrap;
     }
 }
 </style>
