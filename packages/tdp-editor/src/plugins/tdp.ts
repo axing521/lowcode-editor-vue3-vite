de50c745aa85c7dd13a256c52093ab97d52b919b
liuyc14 <liuyc14@lenovo.com>
2023-05-06 17:48:56 +0800

    设计时$tdp的跳转页面方法优化

--- packages/tdp-editor/src/plugins/tdp.ts
+++ packages/tdp-editor/src/plugins/tdp.ts
@@ -2,12 +2,13 @@ import { ref, reactive, computed, watch, watchEffect, type App } from 'vue';
 import type { Router } from 'vue-router';
 import { useAppControler } from 'tdp-editor-common/src/controller';
 
-export default function useTDP(app: App, router: Router) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function useTDP(app: App, router?: Router) {
     const appController = useAppControler(app);
     window.$tdp = {
         App: {
             changePage(pageKey: string) {
-                appController.routerPage({ router, pageKey });
+                appController.changePage(pageKey, '');
             },
         },
         Utils: {},
